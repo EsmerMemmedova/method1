4be684216de3181c86bead1af038a9c77a4b8bab
Esmer <bd7tqjs7c@code.edu.az>
2024-03-17 22:28:10 +0400

    method elave et

---
+++ methods/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // task1; 2 string array-i içerisindeki ortaq elementleri tapib ekrana yazdiran metod;
+            //    String[] newarr = { "Esmer", "Ekber", "Sema" };
+            //    String[] arr = { "Ekber", "Islam", "Gunay" };
+            //    Oratqelementler(arr,newarr);
+        }
+        //public static void Oratqelementler(string[] arr, string[] newarr)
+        //{
+        //    Console.WriteLine("Ortaq elementler: ");
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        for (int j = 0; j < newarr.Length; j++)
+        //            if (arr[i] == newarr[j])
+        //            {
+        //                Console.WriteLine(newarr[j]);
+        //            }
+        //    }
+
+        //}
+
+
+
+
+
+
+
+
+        //task3 Array -in içerisinde verilmiş E herfi ile başlayan elementleri ekrana yazdıran method
+        //    string[] arr = { "Esmer", "Ekber", "Sema" };
+        //    Eherfiilebaslat(  arr);
+        //  
+
+        //}
+
+        //public static void Eherfiilebaslat(string[] arr)
+        //{
+        //    Console.WriteLine("E herfi ie baslayan arqumentler: ");
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+
+        //        if (arr[i].StartsWith("E") || arr[i].StartsWith("e"))
+        //        {
+        //            Console.WriteLine(arr[i]);
+        //        }
+        //    }
+        //}
+
+
+
+
+
+
+        // task2 array daxilindeki elementlerin hasilini tapib ekrana yazdiran metod
+
+        //    int[] newarr = { 1, 2, 3, 4, 5 };
+
+        //    Console.WriteLine("arqumentlerin hasili: " + Arr(newarr));
+
+        //}
+
+        //public static int Arr(int[] array)
+        //{
+        // int  hasil= 1;
+        // for (int i = 0; i < array.Length; i++)
+        //    {
+        //        hasil *= array[i];
+        //    }
+        //    return hasil
+
+    }
+}
+
